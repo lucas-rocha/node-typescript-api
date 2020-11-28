1ae294695a96921c43ad8fcb36e44c027d3b12e0
Lucas Rocha <lucasrocha.dv@gmail.com>
2020-11-28 01:57:41 -0300

    Add eslint and fix problems

--- src/server.ts
+++ src/server.ts
@@ -4,26 +4,25 @@ import { json, Application } from 'express'
 import { ForecastController } from './controllers/forecast'
 
 export class SetupServer extends Server {
-
-  constructor(private port = 3000) {
+  constructor (private port = 3000) {
     super()
   }
 
-  public init(): void {
+  public init (): void {
     this.setupExpress()
     this.setupControllers()
   }
 
-  private setupExpress(): void {
+  private setupExpress (): void {
     this.app.use(json())
   }
 
-  private setupControllers(): void {
+  private setupControllers (): void {
     const forecastController = new ForecastController()
     this.addControllers([forecastController])
   }
 
-  public getApp(): Application {
+  public getApp (): Application {
     return this.app
   }
 }
--- test/globals.d.ts
+++ test/globals.d.ts
@@ -1,6 +1,8 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 declare namespace NodeJS {
-  //https://stackoverflow.com/a/51114250
+  // https://stackoverflow.com/a/51114250
   interface Global {
-    testRequest: import('supertest').SuperTest<import('supertest').Test>
+    testRequest: import('supertest').SuperTest<import('supertest').Test>;
   }
 }
