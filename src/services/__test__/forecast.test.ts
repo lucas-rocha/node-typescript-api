8574ac8eb4f20ec624343ebd4271904f809ce9a1
Lucas Rocha <lucasrocha.dv@gmail.com>
2020-12-10 20:24:22 -0300

    Add group Forecast by time logic

--- src/services/__test__/forecast.test.ts
+++ src/services/__test__/forecast.test.ts
@@ -18,49 +18,64 @@ describe('Forecast Service', () => {
     ]
     const expectedResponse = [
       {
-        lat: -33.792726,
-        lng: 151.289824,
-        name: 'Manly',
-        position: 'E',
-        rating: 1,
-        swellDirection: 174.91,
-        swellHeight: 0.51,
-        swellPeriod: 6.7,
         time: '2020-12-05T00:00:00+00:00',
-        waveDirection: 151.02,
-        waveHeight: 1.94,
-        windDirection: 169.48,
-        windSpeed: 6.56
+        forecast: [
+          {
+            lat: -33.792726,
+            lng: 151.289824,
+            name: 'Manly',
+            position: 'E',
+            rating: 1,
+            swellDirection: 174.91,
+            swellHeight: 0.51,
+            swellPeriod: 6.7,
+            time: '2020-12-05T00:00:00+00:00',
+            waveDirection: 151.02,
+            waveHeight: 1.94,
+            windDirection: 169.48,
+            windSpeed: 6.56
+          }
+        ]
       },
       {
-        lat: -33.792726,
-        lng: 151.289824,
-        name: 'Manly',
-        position: 'E',
-        rating: 1,
-        swellDirection: 175.23,
-        swellHeight: 0.49,
-        swellPeriod: 6.72,
         time: '2020-12-05T01:00:00+00:00',
-        waveDirection: 151.12,
-        waveHeight: 1.88,
-        windDirection: 168.71,
-        windSpeed: 6.27
+        forecast: [
+          {
+            lat: -33.792726,
+            lng: 151.289824,
+            name: 'Manly',
+            position: 'E',
+            rating: 1,
+            swellDirection: 175.23,
+            swellHeight: 0.49,
+            swellPeriod: 6.72,
+            time: '2020-12-05T01:00:00+00:00',
+            waveDirection: 151.12,
+            waveHeight: 1.88,
+            windDirection: 168.71,
+            windSpeed: 6.27
+          }
+        ]
       },
       {
-        lat: -33.792726,
-        lng: 151.289824,
-        name: 'Manly',
-        position: 'E',
-        rating: 1,
-        swellDirection: 175.54,
-        swellHeight: 0.46,
-        swellPeriod: 6.75,
         time: '2020-12-05T02:00:00+00:00',
-        waveDirection: 151.23,
-        waveHeight: 1.81,
-        windDirection: 167.95,
-        windSpeed: 5.98
+        forecast: [
+          {
+            lat: -33.792726,
+            lng: 151.289824,
+            name: 'Manly',
+            position: 'E',
+            rating: 1,
+            swellDirection: 175.54,
+            swellHeight: 0.46,
+            swellPeriod: 6.75,
+            time: '2020-12-05T02:00:00+00:00',
+            waveDirection: 151.23,
+            waveHeight: 1.81,
+            windDirection: 167.95,
+            windSpeed: 5.98
+          }
+        ]
       }
     ]
     const forecast = new Forecast(new StormGlass())
