0caacd302ea322cc228222f31a9e78d74a4a906d
Lucas Rocha <lucasrocha.dv@gmail.com>
2020-12-14 23:59:53 -0300

    Add database init & close config

--- src/server.ts
+++ src/server.ts
@@ -2,19 +2,29 @@ import './util/module-alias'
 import { Server } from '@overnightjs/core'
 import { json, Application } from 'express'
 import { ForecastController } from './controllers/forecast'
+import * as database from '@src/database'
 
 export class SetupServer extends Server {
   constructor (private port = 3000) {
     super()
   }
 
-  public init (): void {
+  public async init (): Promise<void> {
     this.setupExpress()
     this.setupControllers()
+    await this.databaseSetup()
   }
 
   private setupExpress (): void {
     this.app.use(json())
+  }
+
+  private async databaseSetup (): Promise<void> {
+    await database.connect()
+  }
+
+  public async close (): Promise<void> {
+    await database.close()
   }
 
   private setupControllers (): void {
